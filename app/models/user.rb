56d60e62ba1565006869edc565e44e80f599baa9
yuan <qmyemail@gmail.com>
2013-11-04 11:47:08 +0800

    user has many comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :questions, :foreign_key => :questioner_id
   has_many :answers, :foreign_key => :answerer_id
+  has_many :comments, :foreign_key => :commenter_id
 
   def fourm_admin?(forum)
     true

632f2cecd0a3d2b67ff863fa5989eda06e32d29c
MinoWu <minowu@foxmail.com>
2014-03-26 22:56:14 +0800

    重构modal

--- app/assets/javascripts/ask/modal.js
+++ app/assets/javascripts/ask/modal.js
@@ -1,32 +1,9 @@
 $(document).ready(function() {
 
-	'use strict';
+	$('.btn-modal').showModal(function(modal) {
+		$(modal).on('shown.bs.modal', function() {
+			// 添加绑定事件检查 Form validation
+		});
+	});
 
-	// jQuery element
-	var $element = $('.btn-modal');
-
-	// modal template
-	var modal = [
-		'<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">',
-			'<div class="modal-dialog">',
-				'<div class="modal-content">',
-				'</div>',
-			'</div>',
-		'</div>'
-	].join();
-  
-  $element.click(function(e) {
-    
-    var $modal = $(modal)
-    	.modal({
-      	remote: $(this).attr('href')
-    	})
-    	.appendTo($('body'))
-    	// 绑定销毁事件
-    	.on('hidden.bs.modal', function() {
-    		$(this).remove();
-    	});
-
-    e.preventDefault();
-  });
 });

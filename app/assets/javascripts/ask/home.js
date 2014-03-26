bb3a1dc130442f1945102c3f779a0b1c44763a80
MinoWu <minowu@foxmail.com>
2014-03-26 14:23:47 +0800

    添加tableindex，role和aria标签属性，更易被机器识别

--- app/assets/javascripts/ask/home.js
+++ app/assets/javascripts/ask/home.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 	// modal template
 	var modal = [
-		'<div class="modal fade">',
+		'<div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">',
 			'<div class="modal-dialog">',
 				'<div class="modal-content">',
 				'</div>',

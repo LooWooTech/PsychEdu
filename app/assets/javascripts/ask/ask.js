585cc8bfab45b555da166c4c0e3e28fc95b05ddf
MinoWu <minowu@foxmail.com>
2014-03-27 09:49:25 +0800

    优化JS钩子表示方式

--- app/assets/javascripts/ask/ask.js
+++ app/assets/javascripts/ask/ask.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$('.modal-btn-ask').showModal(function(modal) {
+	$('.J_askBtn').showModal(function(modal) {
 		$(modal).on('shown.bs.modal', function() {
 			// 添加绑定事件检查 Form validation
 		});
--- app/assets/javascripts/ask/complaints.js
+++ app/assets/javascripts/ask/complaints.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-	$('.modal-btn-complaints').showModal(function(modal) {
+	$('.J_complaintsBtn').showModal(function(modal) {
 		$(modal).on('shown.bs.modal', function() {
 			// 添加绑定事件检查 Form validation
 		});

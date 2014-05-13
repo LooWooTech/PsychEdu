85520799461c675df7be843d6e3c7faece95635d
MinoWu <minowu@foxmail.com>
2014-05-13 15:55:57 +0800

    开发comments相关的功能

---
+++ app/assets/javascripts/ask/comments.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+	$('.J_commentsBtn').on('click', function(e) {
+
+		e.preventDefault();
+
+		var $this = $(this),
+			$action = $this.parent().parent();
+
+		if ($action.hasClass('open')) {
+			$action.removeClass('open');
+		}
+		else {
+			$action.addClass('open');
+			// 获得 ajax
+			$.get($this.attr('href'), function(data) {
+				var $comments = $(data);
+				$action.find('.action-comments-box').html($comments);
+			});
+		}
+	});
+});
--- app/assets/javascripts/ask/manifest.js
+++ app/assets/javascripts/ask/manifest.js
@@ -7,5 +7,6 @@
 *= require ask/ask
 *= require ask/complaints
 *= require ask/questions
+*= require ask/comments
 *= require ask/ckeditor
 */

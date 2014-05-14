f5807153159a623a584fc2c3ae341e01919156d7
MinoWu <minowu@foxmail.com>
2014-05-14 21:50:13 +0800

    重写 complaints 使用 buildModal

--- app/assets/javascripts/ask/complaints.js
+++ app/assets/javascripts/ask/complaints.js
@@ -1,9 +1,8 @@
 $(document).ready(function() {
-
-	$('.J_complaintsBtn').showModal(function(modal) {
-		$(modal).on('shown.bs.modal', function() {
-			// 添加绑定事件检查 Form validation
-		});
+	$(document).delegate('[data-toggle="complaints"]', 'click', function(e) {
+		e.preventDefault();
+		$.buildModal({
+			remote: $(this).attr('href')
+		}).render();
 	});
-
 });

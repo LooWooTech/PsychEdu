f5ae301e92568fd7c7417fbfed286b51927273f3
MinoWu <minowu@foxmail.com>
2014-05-15 16:17:39 +0800

    重构complaints

--- app/assets/javascripts/ask/complaints.js
+++ app/assets/javascripts/ask/complaints.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
-	$(document).delegate('[data-toggle="complaints"]', 'click', function(e) {
-		e.preventDefault();
-		$.buildModal({
-			remote: $(this).attr('href')
-		}).render();
+	// $('#mainarea').delegate('[data-event="complaints"]', 'click', function(e) {
+	// 	e.preventDefault();
+	// 	$.buildModal({
+	// 		remote: $(this).attr('href')
+	// 	}).render();
+	// });
+
+	$('#mainarea').delegate('[data-event="complaints"]', 'ajax:success', function(e, data) {
+		console.log(data);
 	});
 });

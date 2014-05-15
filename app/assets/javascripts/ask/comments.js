253023c291a6fa6fe3d3a85ac723a02138ecf716
MinoWu <minowu@foxmail.com>
2014-05-15 17:11:31 +0800

    改写 show comments 的，监听 ajax:success 事件

--- app/assets/javascripts/ask/comments.js
+++ app/assets/javascripts/ask/comments.js
@@ -3,30 +3,19 @@ $(document).ready(function() {
 	$el = $('#mainarea');
 
 	// show comments
-	$el.delegate('[data-event="comments"]', 'click', function(e) {
-		e.preventDefault();
-
-		var $this = $(this),
-			$action = $this.parent().parent();
-
-		if ($action.hasClass('open')) {
-			$action.removeClass('open');
-		}
-		else {
-			$action.addClass('open');
-			// 获得 ajax
-			$.get($this.attr('href'), function(data) {
-				var $comments = $(data);
-				$action.find('.action-comments-box').html($comments);
-			});
-		}
+	$el.delegate('[data-event="comments"]', 'ajax:success', function(e, data) {
+		$(this)
+			.closest('.actions')
+			.toggleClass('open')
+			.find('.action-comments-box')
+			.html(data);
 	});
 
 	// remove comment
 	$el.delegate('[data-event="remove"]', 'ajax:success', function(e, data) {
 		$(this)
 			.closest('.media')
-			.fadeOut('slow', function() {
+			.fadeOut(function() {
 				$(this).remove();
 			});
 	});

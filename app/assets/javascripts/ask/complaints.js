c9acbd4048e47b1179163faacd21a6f3641c40cc
MinoWu <minowu@foxmail.com>
2014-05-15 23:13:10 +0800

    重构 complaints 的提交

--- app/assets/javascripts/ask/complaints.js
+++ app/assets/javascripts/ask/complaints.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
 	$('#mainarea').delegate('[data-event="complaints"]', 'ajax:success', function(e, data) {
 		var modal = $(data).buildModal();
+
+		// publish edited answer
+		modal.delegate('[data-event="complaint.publish"]', 'ajax:success', function(e, data) {
+			alert(data);
+			modal.modal('hide');
+		});
 	});
 });

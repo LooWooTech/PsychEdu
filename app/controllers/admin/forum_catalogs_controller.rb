44f2acb41bf3f0f1dd9cd4543ce36b9156a1c523
yuan <qmyemail@gmail.com>
2014-05-19 09:07:17 +0800

    edit forum catalog

--- app/controllers/admin/forum_catalogs_controller.rb
+++ app/controllers/admin/forum_catalogs_controller.rb
@@ -1,5 +1,7 @@
 module Admin
   class ForumCatalogsController < AdminController
+
+    before_action :find_forum_catalog, :only => [:edit, :update, :destroy]
     def new
       @forum_catalog = ForumCatalog.new
     end
@@ -7,7 +9,7 @@ module Admin
     def create
       @forum_catalog = ForumCatalog.new catalog_params
       if @forum_catalog.save
-        flash[:notice] = "创建了社区版块分类#{@forum_catalog.name}"
+        flash[:notice] = "创建了社区版块分类 #{@forum_catalog.name}"
         redirect_to forum_catalogs_path
       else
         render :new
@@ -18,7 +20,24 @@ module Admin
       @forum_catalogs = ForumCatalog.all
     end
 
+    def edit
+      render :new
+    end
+
+    def update
+      if @forum_catalog.update_attributes catalog_params
+        flash[:notice] = "修改了社区版块分类 #{@forum_catalog.name}"
+        redirect_to forum_catalogs_path
+      else
+        render :new
+      end
+    end
+
     private
+
+    def find_forum_catalog
+      @forum_catalog = ForumCatalog.find params[:id]
+    end
 
     def catalog_params
       params.require(:forum_catalog).permit(:name)

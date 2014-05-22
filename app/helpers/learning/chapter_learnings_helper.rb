cbed34b449a1b684306533137fc069329f92cbff
MinoWu <minowu@foxmail.com>
2014-05-22 21:42:54 +0800

    开发 timeline entry

--- app/helpers/learning/chapter_learnings_helper.rb
+++ app/helpers/learning/chapter_learnings_helper.rb
@@ -3,12 +3,12 @@ module Learning
     def unit_exam_link(unit_learning)
       if unit_learning.ready_for_exam?
         if unit_learning.unfinished_exam
-          link_to '继续上次练习', unit_exam_path(unit_learning.unfinished_exam), :class => 'start-exam'
+          link_to '继续上次练习', unit_exam_path(unit_learning.unfinished_exam), :class => 'btn btn-sm btn-inverse'
         else
           if unit_learning.exam_limited?
-            raw "<span class='start-exam'>开始练习</span>"
+            raw "<span class='btn btn-sm btn-inverse'>开始练习</span>"
           else
-            link_to '开始练习', unit_learning_unit_exams_path(unit_learning), :method => :post, :class => "start-exam"
+            link_to '开始练习', unit_learning_unit_exams_path(unit_learning), :method => :post, :class => "btn btn-sm btn-inverse"
           end
         end
       end

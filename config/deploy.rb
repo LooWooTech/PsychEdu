0c8daf0d7baf1038b1bdc7b21298b981db57e71d
yuan <qmyemail@gmail.com>
2014-06-13 11:34:07 +0800

    recompile all assets everytime

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,6 +65,7 @@ task :deploy => :environment do
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
     invoke :'bower:install'
+    invoke :'assets:clobber'
     invoke :'rails:assets_precompile'
 
     to :launch do

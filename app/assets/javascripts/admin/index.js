d46f4a22f41daa9adbc9d3681e4a543ff8fa9bf2
yuan <qmyemail@gmail.com>
2014-07-01 21:08:05 +0800

    fix wysihtml5 editor

--- app/assets/javascripts/admin/index.js
+++ app/assets/javascripts/admin/index.js
@@ -5,7 +5,8 @@
 *= require jquery-file-upload/vendor/jquery.ui.widget
 *= require jquery-file-upload/jquery.fileupload
 *= require jquery-validation
-*= require wysihtml5
+*= require wysihtml5/wysihtml5
+*= require wysihtml5/advanced_parser_rule
 *= require '../datepicker'
 *= require_tree .
 */
